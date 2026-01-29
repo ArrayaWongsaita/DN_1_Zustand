751e1137686e3b809e025fbf694adb49c05f94db
ArrayaWongsaita <Wongsaita.a@gmail.com>
2026-01-29 16:05:26 +0700

    feat: implement MainLayout with navigation and create counter store file

--- src/layouts/Main.layout.tsx
+++ src/layouts/Main.layout.tsx
@@ -1,3 +1,15 @@
+import { Link, Outlet } from "react-router";
+
 export default function MainLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <header className="flex justify-between w-full bg-blue-300">
+        <Link to="/">HOME</Link>
+        <nav>
+          <Link to="/counter">counter</Link>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,14 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/Main.layout";
+import CounterPage from "../features/counter/Counter.page";
 
 export const router = createBrowserRouter([
-  { path: "/", children: [{ index: true, element: <div>home</div> }] },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <div>home</div> },
+      { path: "counter", element: <CounterPage /> },
+    ],
+  },
 ]);
